e9804f2a87265f7169bd6fce8f2b7ff2606a9f89
Dylan Katchen <89935431+dylankatchen@users.noreply.github.com>
2026-02-11 10:53:53 -0500

    hello

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -61,7 +61,7 @@ function App() {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                 </svg>
               </div>
-              <span className="font-bold text-xl tracking-tight text-slate-900">ResumeGrader<span className="text-indigo-600">.ai</span></span>
+              <span className="font-bold text-xl tracking-tight text-slate-900">Dylan's ResumeGrader<span className="text-indigo-600">.ai</span></span>
             </div>
           </div>
         </div>
